76b85e569cd1368d0e1ef0393ba2fd62470b4fa9
gugovind <gugovind@cisco.com>
2017-06-23 08:50:57 -0700

    static csv file and server page with todo

---
+++ server/server.js
@@ -0,0 +1,8 @@
+/**
+1. From the CSV file generate a list of all the cities in the world and their latitude longitude
+2. From the static list of capitals generate a list.
+    * This makes the first level
+3. Create a REST API method that will return the list of 10 cities and latlngs given the level.
+4. This list above should be decided based on game level where we pick random cities from the US, UK, AUS, FR, GER, SPAIN, PORTUGAL.
+5. Create another REST API which when given a couple of latlngs will calculate crow flies.
+*/
