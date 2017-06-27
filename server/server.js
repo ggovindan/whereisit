4564b26fecffc473b3cfc3cd12db9f13449d3b6a
ggovindan <gxguru@gmail.com>
2017-06-26 17:12:19 -0700

    add modules to be used for lat long

--- server/server.js
+++ server/server.js
@@ -6,3 +6,29 @@
 4. This list above should be decided based on game level where we pick random cities from the US, UK, AUS, FR, GER, SPAIN, PORTUGAL.
 5. Create another REST API which when given a couple of latlngs will calculate crow flies.
 */
+
+// utility to calculate geo-distance
+const geo = require('node-geo-distance');
+const Promise = require('bluebird');
+
+/**
+// White house
+var coord1 = {
+  latitude: 38.8977330,
+  longitude: -77.0365310
+}
+
+// Washington Monument
+var coord2 = {
+  latitude: 38.8894840,
+  longitude: -77.0352790
+}
+
+geo.vincenty(coord1, coord2, function(dist) {
+  console.log(dist);
+});
+*/
+
+/**
+Utility to get all the cities and the coordinates into an array
+*/
