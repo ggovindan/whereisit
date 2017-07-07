80f3f87020235ce4840d2965f7585a573bf86d91
ggovindan <gxguru@gmail.com>
2017-07-07 12:06:27 -0700

    changes to host the webpage as server

--- middleware/index.js
+++ middleware/index.js
@@ -1,38 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const latlng = require('./latlng-util');
+const latlng = require('../server/latlng-util');
 
-const app = express();
 const router = express.Router();
 
 router.use(bodyParser.json());
 
-// anythin with /api/v1 route it to our router
-app.use('/api/v1', router);
-
-// /api/v1/ping
-router.get('/ping', (req, res) => {
+exports.ping = (req, res) => {
+  console.log('hitting ping');
   res.status(200).send({
     version: '1.0.0',
   })
-});
+};
 
-// /api/v1/game:level
-router.get('/game:level', function(req, res) {
+exports.newGame = (req, res) => {
   const level = req.params.level;
   console.log('level=', typeof level);
+  console.log('level dip=', level);
   console.time('getting_cities');
   res.status(200).send(JSON.parse(JSON.stringify({cities: latlng.getRandomList(parseInt(level))})));
   console.timeEnd('getting_cities');
-});
+}
 
-// /api/v1/score
-router.post('/score', function(req, res) {
+exports.score = (req, res) => {
   const coord1 = req.body.coord1;
   const coord2 = req.body.coord2;
   latlng.getScore(coord1, coord2, (score) => {
     res.status(200).send({score: score});
   });
-});
+}
 
-module.exports = app;
+exports.router = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const apiRoutes = require('./middleware');
+
+var app = module.exports = express();
+
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname+'/views');
+app.set('view engine', 'html');
+app.use(express.static(__dirname + '/'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/game:level', apiRoutes.newGame);
+
+app.get('/ping', apiRoutes.ping);
+
+app.post('/score', apiRoutes.score);
+
+app.listen(process.env.PORT || 5000, function() {
+  console.log('listening on http://localhost:5000');
+});
